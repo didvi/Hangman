5514e8a6738624f5a3f59a91fa074c4f20b9a58a
Sophia Song <sophies1234@gmail.com>
2019-02-26 16:04:20 -0800

    added images

--- Hangman/Hangman/Controller/HangmanViewController.swift
+++ Hangman/Hangman/Controller/HangmanViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class HangmanViewController: UIViewController {
 
     let hangman = HangMan()
+    var hangmanImage = UIImage(named: "hangman1")
+    
+    
+    @IBOutlet weak var MyLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -22,12 +26,37 @@ class HangmanViewController: UIViewController {
 
     @IBAction func keyClick(_ sender: UIButton) {
         var letterPressed = sender.title(for: .normal)
-        if hangman.isInSecretWord(guess letterPressed) {
-            label.text = hangman.update(letterPressed)
+        if hangman.isInSecretWord(letterPressed) {
+            MyLabel.text = hangman.update(letterPressed)
         }
         else {
-            hangman.guessesRemaining -= 1;
-            
+            hangman.badGuesses += 1;
+        }
+    }
+
+
+    
+    func changeImage() {
+        if hangman.badGuesses == 0 {
+            hangmanImage = UIImage(named: "hangman1")
+        }
+        if hangman.badGuesses == 1 {
+            hangmanImage = UIImage(named: "hangman2")
+        }
+        if hangman.badGuesses == 2 {
+            hangmanImage = UIImage(named: "hangman3")
+        }
+        if hangman.badGuesses == 3 {
+            hangmanImage = UIImage(named: "hangman4")
+        }
+        if hangman.badGuesses == 4 {
+            hangmanImage = UIImage(named: "hangman5")
+        }
+        if hangman.badGuesses == 5 {
+            hangmanImage = UIImage(named: "hangman6")
+        }
+        if hangman.badGuesses == 6 {
+            hangmanImage = UIImage(named: "hangman7")
         }
     }
 }
