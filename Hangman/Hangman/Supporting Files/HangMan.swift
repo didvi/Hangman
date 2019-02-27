36fe4bb1617e85b71a6db2e0ea9b6ea0bff4a2a2
didvi <divi@berkeley.edu>
2019-02-26 16:07:04 -0800

    fixed update function for hangman class

--- Hangman/Hangman/Supporting Files/HangMan.swift
+++ Hangman/Hangman/Supporting Files/HangMan.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class HangMan {
     let secretWord: String;
-    var secretWordArray: Array<String?>;
+    let secretWordArray: Array<Character>;
+    var displayArray: Array<Character?>;
     var badGuesses: Int;
     
     let wordBank = ["awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "kiosk", "klutz", "memento", "mystify", "numbskull", "ostracize", "oxygen", "pajama", "phlegm", "pixel", "polka", "quad",  "quip" ,"rhythmic" ,"rogue" ,"sphinx","squawk" ,"swivel" ,"toady" ,"twelfth" ,"unzip" ,"waxy" ,"wildebeest" ,"yacht" ,"zealous" ,"zigzag" ,"zippy", "zombie"]
@@ -20,11 +21,13 @@ class HangMan {
     init() {
         let randnum: Int = Int.random(in: 0..<wordBank.count)
         self.secretWord = wordBank[randnum]
-        self.secretWordArray = Array(repeating: nil, count: secretWord.count )
+        self.secretWordArray = Array(secretWord)
+        self.displayArray = Array(repeating: "_", count: secretWord.count)
         self.badGuesses = 0
     }
     
-    func isInSecretWord(guess g: String) -> Bool {
+    // doesn't account for capitalization
+    func isInSecretWord(_ g: String) -> Bool {
         let charset = CharacterSet(charactersIn: self.secretWord)
         if self.secretWord.rangeOfCharacter(from: charset) != nil {
             return true;
@@ -33,10 +36,16 @@ class HangMan {
     }
     
     // outputs a new word array to render to the screen
-    func update(guess g: String) {
+    func update(_ g: Character) {
+        
+        for i in 0..<secretWordArray.count {
+            if secretWordArray[i] == g {
+                displayArray[i] = g
+            }
+        }
+        
         
     }
-        
     
     
     
